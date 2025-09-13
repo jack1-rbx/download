a677fc97b887121c0dba36dc7436361a66e3a992
jack1-rbx <officerjcd@gmail.com>
2025-09-13 17:07:31 +0100

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 
-window.onload = function () {
+export default function DownloadPage() {
+  useEffect(() => {
     const fileUrl = "https://github.com/jack1-rbx/download/raw/refs/heads/main/api/RO-SCH%20STARTER%20PACK.rbxm";
 
     const a = document.createElement("a");
     a.href = fileUrl;
     a.download = "RO-SCH STARTER PACK.rbxm"; 
     document.body.appendChild(a);
-
     a.click();
-
     document.body.removeChild(a);
-};
+  }, []);
+
+  return (
+    <div>
+      <h1>Preparing your download...</h1>
+    </div>
+  );
+}
