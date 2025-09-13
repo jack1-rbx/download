98fb079b6409ea73f89ff6db7d0f134ab483ed74
jack1-rbx <officerjcd@gmail.com>
2025-09-13 17:02:10 +0100

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,1 +1,13 @@
 
+window.onload = function () {
+    const fileUrl = "https://raw.githubusercontent.com/<USERNAME>/<REPOSITORY>/main/API/RO-SCH%20STARTER%20PACK.rbxm";
+
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = "RO-SCH STARTER PACK.rbxm"; 
+    document.body.appendChild(a);
+
+    a.click();
+
+    document.body.removeChild(a);
+};
